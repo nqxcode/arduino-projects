e763621d547e4d33538ce3120d95f7630c8d72c5
Andrey Pokoev <nc101ux@gmail.com>
2017-09-14 09:50:04 +0300

    Update thermometer.ino

--- thermometer/thermometer.ino
+++ thermometer/thermometer.ino
@@ -1,98 +1,65 @@
 #include <LiquidCrystal.h>
 #include <math.h>
 
-// к какому аналоговому пину мы подключены
+#define THERMISTORPIN A0 // к какому аналоговому пину мы подключены
+#define THERMISTORNOMINAL 100000 // сопротивление при 25 градусах по Цельсию
+#define TEMPERATURENOMINAL 25 // temp. для номинального сопротивления (практически всегда равна 25 C)
+#define NUMSAMPLES 5 // сколько показаний используем для определения среднего значения
+#define BCOEFFICIENT 3950 // бета коэффициент термистора (обычно 3000-4000)
+#define SERIESRESISTOR 100000 // сопротивление второго резистора
 
-#define THERMISTORPIN A0
-
-// сопротивление при 25 градусах по Цельсию
-
-#define THERMISTORNOMINAL 100000
-
-// temp. для номинального сопротивления (практически всегда равна 25 C)
-
-#define TEMPERATURENOMINAL 25
-
-// сколько показаний используем для определения среднего значения
-
-#define NUMSAMPLES 5
-
-// бета коэффициент термистора (обычно 3000-4000)
-
-#define BCOEFFICIENT 3950
-
-// сопротивление второго резистора
-
-#define SERIESRESISTOR 100000
-
-int samples[NUMSAMPLES];
+int measurements[NUMSAMPLES];
 
 LiquidCrystal lcd(12, 11, 10, 5, 4, 3, 2); // инициализируем LCD
 
-
 void setup(void) {
+  Serial.begin(9600);
+
   lcd.begin(16, 2);
   lcd.clear();
   lcd.setCursor(0, 0);
-
-  Serial.begin(9600);
-
 }
 
 void loop(void) {
   uint8_t i;
 
-  float average;
+  float averageMeasurement;
+  float temperature;
+  float resistence;
 
-  // сводим показания в вектор с небольшой задержкой между снятием показаний
-
+  // снимаем показания несколько раз с небольшой задержкой между снятием показаний
   for (i = 0; i < NUMSAMPLES; i++) {
-
-    samples[i] = analogRead(THERMISTORPIN);
-
+    measurements[i] = analogRead(THERMISTORPIN);
     delay(10);
-
   }
 
   // рассчитываем среднее значение
-
-  average = 0;
-
+  averageMeasurement = 0;
   for (i = 0; i < NUMSAMPLES; i++) {
-
-    average += samples[i];
-
+    averageMeasurement += measurements[i];
   }
 
-  average /= NUMSAMPLES;
+  averageMeasurement /= NUMSAMPLES;
 
   // конвертируем значение в сопротивление
+  resistence = 1023 / averageMeasurement - 1;
+  resistence = SERIESRESISTOR / resistence;
 
-  average = 1023 / average - 1;
+  // конвертируем значение в температуру
+  temperature = resistence / THERMISTORNOMINAL; // (R/Ro)
+  temperature = log(temperature); // ln(R/Ro)
+  temperature /= BCOEFFICIENT; // 1/B * ln(R/Ro)
+  temperature += 1.0 / (TEMPERATURENOMINAL + 273.15); // + (1/To)
 
-  average = SERIESRESISTOR / average;
-
-  float steinhart;
-
-  steinhart = average / THERMISTORNOMINAL; // (R/Ro)
-
-  steinhart = log(steinhart); // ln(R/Ro)
-
-  steinhart /= BCOEFFICIENT; // 1/B * ln(R/Ro)
-
-  steinhart += 1.0 / (TEMPERATURENOMINAL + 273.15); // + (1/To)
-
-  steinhart = 1.0 / steinhart; // инвертируем
-
-  steinhart -= 273.15; // конвертируем в градусы по Цельсию
+  temperature = 1.0 / temperature; // инвертируем
+  temperature -= 273.15; // конвертируем в градусы по Цельсию
 
   lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print("Temperature is:");
   lcd.setCursor(0, 1);
-  lcd.print(steinhart);
+  lcd.print(temperature);
   lcd.print(" C");
 
   delay(500);
 }
-
