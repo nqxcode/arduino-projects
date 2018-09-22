9888a3d4cad8bb8629f41465ac18157e6824d36b
nqxcode <nc101ux@gmail.com>
2018-09-22 16:15:25 +0300

    Added files

--- crawler/bluetooth_crawler.ino
+++ crawler/bluetooth_crawler.ino
@@ -1,3 +1,24 @@
+<<<<<<< HEAD
+#include <SoftwareSerial.h>
+#include "crawler.h"
+
+
+// Connect the HC-06 TX to the Arduino RX on pin 2.
+// Connect the HC-06 RX to the Arduino TX on pin 3 through a voltage divider.
+SoftwareSerial BTserial(2, 3); // RX | TX
+
+Crawler crawler(10, 12, 11, 13); // E1, M1, E2, M2
+
+String commandString = "";
+
+void setup()
+{
+  Serial.begin(9600);
+  BTserial.begin(9600);
+  crawler.begin();
+
+  commandString.reserve(200);
+=======
 int E1 = 10;
 int M1 = 12;
 int E2 = 11;
@@ -62,10 +83,49 @@ void setup()
   pinMode(M2, OUTPUT);
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
+>>>>>>> 5c059c02adfa68637972e711d902512e4950c7ba
 }
 
 void loop()
 {
+<<<<<<< HEAD
+  // Keep reading from HC-06 and send to Arduino Serial Monitor
+  while (BTserial.available())
+  {
+    char c = BTserial.read();
+
+    if (c != ';') {
+      commandString += c;
+      continue;
+    }
+    execute(commandString);
+    commandString = "";
+  }
+}
+
+void execute(String commandString)
+{
+  Serial.print("Input command: ");
+  Serial.println(commandString);
+
+  if (commandString == "right") {
+    crawler.rotate(0);
+
+  } else if (commandString == "left") {
+    crawler.rotate(1);
+
+  } else if (commandString == "go") {
+    crawler.go();
+
+  } else if (commandString == "back") {
+    crawler.back();
+
+  } else if (commandString == "wait") {
+    crawler.wait();
+
+  } else {
+    Serial.println("Incorrect command!");
+=======
   int waitFirst = true;
   int direction = 0;
   
@@ -84,5 +144,7 @@ void loop()
       rotate(direction);
       delay(200);
     }
+>>>>>>> 5c059c02adfa68637972e711d902512e4950c7ba
   }
 }
+
