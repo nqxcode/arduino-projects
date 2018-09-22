e9566aed49b0ca6c37c9724eb94238545346f1bb
Andrey Pokoev <nc101ux@gmail.com>
2018-09-22 16:17:07 +0300

    Update bluetooth_crawler.ino

--- crawler/bluetooth_crawler.ino
+++ crawler/bluetooth_crawler.ino
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <SoftwareSerial.h>
 #include "crawler.h"
 
@@ -18,77 +17,10 @@ void setup()
   crawler.begin();
 
   commandString.reserve(200);
-=======
-int E1 = 10;
-int M1 = 12;
-int E2 = 11;
-int M2 = 13;
-
-int echoPin = 9;
-int trigPin = 8;
-
-int MIN_DISTANCE = 80; // в сантиметрах
-
-int getDistance()
-{
-  int duration, cm;
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-  duration = pulseIn(echoPin, HIGH);
-  return duration / 58;
-}
-
-void go()
-{
-  digitalWrite(M1, HIGH);
-  digitalWrite(M2, LOW);
-  analogWrite(E1, 255);
-  analogWrite(E2, 255);
-}
-
-void back()
-{
-  digitalWrite(M1, LOW);
-  digitalWrite(M2, HIGH);
-  analogWrite(E1, 200);
-  analogWrite(E2, 200);
-}
-
-void rotate(int direction)
-{
-  if (direction == 1) {
-    digitalWrite(M1, LOW);
-    digitalWrite(M2, LOW);
-  } else {
-    digitalWrite(M1, HIGH);
-    digitalWrite(M2, HIGH);
-  }
-  analogWrite(E1, 200);
-  analogWrite(E2, 200);
-}
-
-void wait()
-{
-  analogWrite(E1, 0);
-  analogWrite(E2, 0);
-}
-
-void setup()
-{
-  Serial.begin (9600);
-  pinMode(M1, OUTPUT);
-  pinMode(M2, OUTPUT);
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
->>>>>>> 5c059c02adfa68637972e711d902512e4950c7ba
 }
 
 void loop()
 {
-<<<<<<< HEAD
   // Keep reading from HC-06 and send to Arduino Serial Monitor
   while (BTserial.available())
   {
@@ -125,26 +57,5 @@ void execute(String commandString)
 
   } else {
     Serial.println("Incorrect command!");
-=======
-  int waitFirst = true;
-  int direction = 0;
-  
-  while (true) {
-    int distance = getDistance();
-    Serial.print(distance);
-    Serial.println(" cm");
-    
-    if (distance > MIN_DISTANCE) {
-      go();
-      break;
-      
-    } else {
-      wait();
-      delay(500);
-      rotate(direction);
-      delay(200);
-    }
->>>>>>> 5c059c02adfa68637972e711d902512e4950c7ba
   }
 }
-
