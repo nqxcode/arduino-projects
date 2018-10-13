6fd831627521e14b012c2f02cf625f89eeec1166
Andrey Pokoev <nc101ux@gmail.com>
2018-10-13 05:41:47 +0300

    Update crawler.cpp

--- crawler/joystick_crawler/crawler.cpp
+++ crawler/joystick_crawler/crawler.cpp
@@ -1,88 +1,89 @@
 /***************************************************
-  This is a library for controller
+  This is a library for crawler
  ****************************************************/
 
 #include <Arduino.h>
 #include "crawler.h"
-#include "controller.h"
 
-Controller::Controller(void) {
+Crawler::Crawler(unsigned int e1, unsigned int m1, unsigned int e2, unsigned int m2, unsigned int buzzer)
+  : E1(e1), M1(m1), E2(e2), M2(m2), Buzzer(buzzer) {
 }
 
-void Controller::setAxes(int xAxis, int yAxis) {
-  // Y-axis used for forward and backward control
-  if (yAxis < 470) {
-    // Set motors backward
-    this->direction = Crawler::Direction::backward;
+void Crawler::begin(void) {
+  pinMode(this->M1, OUTPUT);
+  pinMode(this->M2, OUTPUT);
+  pinMode(this->Buzzer, OUTPUT);
+}
 
-    // Convert the declining Y-axis readings for going backward from 470 to 0 into 0 to 255 value for the PWM signal for increasing the motor speed
-    this->leftSpeed = map(yAxis, 470, 0, 0, 255);
-    this->rightSpeed = map(yAxis, 470, 0, 0, 255);
-
-  }
-  else if (yAxis > 550) {
-    // Set motors forward
-    this->direction = Crawler::Direction::forward;
-
-    // Convert the increasing Y-axis readings for going forward from 550 to 1023 into 0 to 255 value for the PWM signal for increasing the motor speed
-    this->leftSpeed = map(yAxis, 550, 1023, 0, 255);
-    this->rightSpeed = map(yAxis, 550, 1023, 0, 255);
-
-  }
-  // If joystick stays in middle the motors are not moving
-  else {
-    this->leftSpeed = 0;
-    this->rightSpeed = 0;
-  }
-
-  // X-axis used for left and right control
-  if (xAxis < 470) {
-    // Convert the declining X-axis readings from 470 to 0 into increasing 0 to 255 value
-    int xMapped = map(xAxis, 470, 0, 0, 255);
-
-    // Move to left - decrease left motor speed, increase right motor speed
-    this->leftSpeed = this->leftSpeed - xMapped;
-    this->rightSpeed = this->rightSpeed + xMapped;
-
-    // Confine the range from 0 to 255
-    if (this->leftSpeed < 0) {
-      this->leftSpeed = 0;
-    }
-    if (this->rightSpeed > 255) {
-      this->rightSpeed = 255;
-    }
-  }
-  if (xAxis > 550) {
-    // Convert the increasing X-axis readings from 550 to 1023 into 0 to 255 value
-    int xMapped = map(xAxis, 550, 1023, 0, 255);
-    // Move right - decrease right motor speed, increase left motor speed
-    this->leftSpeed = this->leftSpeed + xMapped;
-    this->rightSpeed = this->rightSpeed - xMapped;
-    // Confine the range from 0 to 255
-    if (this->leftSpeed > 255) {
-      this->leftSpeed = 255;
-    }
-    if (this->rightSpeed < 0) {
-      this->rightSpeed = 0;
-    }
-  }
-  // Prevent buzzing at low speeds (Adjust according to your motors. My motors couldn't start moving if PWM value was below value of 70)
-  if (this->leftSpeed < 70) {
-    this->leftSpeed = 0;
-  }
-  if (this->rightSpeed < 70) {
-    this->rightSpeed = 0;
+void Crawler::debug(String message)
+{
+  if (debugMode) {
+    Serial.print("[Crawler debug mode]: ");
+    Serial.println(message);
   }
 }
 
-Crawler::Direction Controller::getDirection() {
-  return this->direction;
+void Crawler::run(Crawler::Direction direction)
+{
+  switch (direction) {
+    case Crawler::Direction::forward:
+      this->debug("forward");
+
+      digitalWrite(M1, HIGH);
+      digitalWrite(M2, LOW);
+      break;
+
+    case Crawler::Direction::backward:
+      this->debug("backward");
+
+      digitalWrite(M1, LOW);
+      digitalWrite(M2, HIGH);
+      break;
+
+    case Crawler::Direction::left_rotation:
+      this->debug("left rotation");
+
+      digitalWrite(M1, LOW);
+      digitalWrite(M2, LOW);
+      break;
+
+    case Crawler::Direction::right_rotation:
+      this->debug("right rotation");
+
+      digitalWrite(M1, HIGH);
+      digitalWrite(M2, HIGH);
+      break;
+
+    default:
+      this->debug("Incorrect direction");
+      return;
+  }
+
+  this->refreshSpeed();
 }
 
-int Controller::getLeftSpeed() {
-  return this->leftSpeed;
+void Crawler::speed(unsigned int leftSpeed, unsigned int rightSpeed)
+{
+  this->debug(String("left speed: ") + String(leftSpeed));
+  this->debug(String("right speed: ") + String(rightSpeed));
+
+  this->leftSpeed = leftSpeed;
+  this->rightSpeed = rightSpeed;
+
+  this->refreshSpeed();
 }
 
-int Controller::getRightSpeed() {
-  return this->rightSpeed;
+void Crawler::refreshSpeed(void)
+{
+  analogWrite(E2, this->rightSpeed);
+  analogWrite(E1, this->leftSpeed);
+}
+
+void Crawler::buzz(void)
+{
+  tone (this->Buzzer, 500);
+  delay(100);
+  tone(this->Buzzer, 1000);
+  delay(100);
+  noTone(this->Buzzer);
 }
