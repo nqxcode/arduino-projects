fc9a15f49a8d18060337cfd273cbe1890d82c8a3
Andrey Pokoev <nc101ux@gmail.com>
2018-10-13 03:30:50 +0300

    Update transmitter.ino

--- crawler/joystick_crawler/transmitter.ino
+++ crawler/joystick_crawler/transmitter.ino
@@ -1,3 +1,4 @@
+
 /*
    Arduino Robot Car Wireless Control using the NRF24L01 Transceiver module
              == Transmitter - Joystick ==
@@ -7,26 +8,45 @@
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
-RF24 radio(8, 9); // CE, CSN
-const byte address[6] = "00001";
-char xyData[32] = "";
+
+RF24 radio(9, 10); // "создать" модуль на пинах 9 и 10 Для Уно
+byte address[][6] = {"1Node", "2Node", "3Node", "4Node", "5Node", "6Node"}; //возможные номера труб
+
+char commandData[32] = "";
 String xAxis, yAxis;
+
+
 void setup() {
   Serial.begin(9600);
-  radio.begin();
-  radio.openWritingPipe(address);
-  radio.setPALevel(RF24_PA_MIN);
-  radio.stopListening();
+
+  radio.begin(); //активировать модуль
+  radio.setAutoAck(1);         //режим подтверждения приёма, 1 вкл 0 выкл
+  radio.setRetries(0, 15);    //(время между попыткой достучаться, число попыток)
+  radio.enableAckPayload();    //разрешить отсылку данных в ответ на входящий сигнал
+  radio.setPayloadSize(32);     //размер пакета, в байтах
+
+  radio.openWritingPipe(address[0]);   //мы - труба 0, открываем канал для передачи данных
+  radio.setChannel(0x60);  //выбираем канал (в котором нет шумов!)
+
+  radio.setPALevel (RF24_PA_MAX); //уровень мощности передатчика. На выбор RF24_PA_MIN, RF24_PA_LOW, RF24_PA_HIGH, RF24_PA_MAX
+  radio.setDataRate (RF24_250KBPS); //скорость обмена. На выбор RF24_2MBPS, RF24_1MBPS, RF24_250KBPS
+  //должна быть одинакова на приёмнике и передатчике!
+  //при самой низкой скорости имеем самую высокую чувствительность и дальность!!
+
+  radio.powerUp(); //начать работу
+  radio.stopListening(); //не слушаем радиоэфир, мы передатчик
 }
+
+
 void loop() {
 
-  xAxis = analogRead(A0); // Read Joysticks X-axis
-  yAxis = analogRead(A1); // Read Joysticks Y-axis
-  // X value
-  xAxis.toCharArray(xyData, 5); // Put the String (X Value) into a character array
-  radio.write(&xyData, sizeof(xyData)); // Send the array data (X value) to the other NRF24L01 modile
-  // Y value
-  yAxis.toCharArray(xyData, 5);
-  radio.write(&xyData, sizeof(xyData));
-  delay(20);
+  xAxis = analogRead(A1); // Read Joysticks X-axis
+  yAxis = analogRead(A2); // Read Joysticks Y-axis
+
+  String command = "xy:" + xAxis + "," + yAxis + ";";
+ 
+  command.toCharArray(commandData, 14);
+  radio.write(&commandData, sizeof(commandData)); // Send the array data (command) to the other NRF24L01 module
+
+  delay(10);
 }
