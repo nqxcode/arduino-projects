ba6fbcad88d7c68bfc20ac47e6a5e63f078e9c9f
Andrey Pokoev <nc101ux@gmail.com>
2018-10-13 05:42:07 +0300

    Update crawler.h

--- crawler/joystick_crawler/crawler.h
+++ crawler/joystick_crawler/crawler.h
@@ -1,15 +1,31 @@
 /****************************************************/
 
-class Controller {
+class Crawler {
   public:
-    Controller::Controller(void);
-    void setAxes(int x, int y);
-    Crawler::Direction getDirection();
-    int getLeftSpeed();
-    int getRightSpeed();
+    enum class Direction
+    {
+      forward, backward, left_rotation, right_rotation
+    };
+
+    Crawler::Crawler(unsigned int e1, unsigned int m1, unsigned int e2, unsigned int m2, unsigned int buzzer);
+    void begin();
+    void debug(String message);
+
+    void run(Direction direction);
+    void speed(unsigned int leftSpeed, unsigned int rightSpeed);
+    void buzz(void);
 
   private:
-    Crawler::Direction direction;
-    int leftSpeed;
-    int rightSpeed;
+    unsigned int E1;
+    unsigned int M1;
+    unsigned int E2;
+    unsigned int M2;
+    unsigned int Buzzer;
+
+    unsigned int leftSpeed;
+    unsigned int rightSpeed;
+
+    void refreshSpeed(void);
+
+    bool debugMode = false;
 };
