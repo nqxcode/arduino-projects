254e897ca29b7b1fb8ad54a5a6ee243c6659f2b1
Andrey Pokoev <nc101ux@gmail.com>
2018-10-13 05:40:46 +0300

    Change bluetooth to radio control

--- crawler/joystick_crawler/joystick_crawler.ino
+++ crawler/joystick_crawler/joystick_crawler.ino
@@ -1,61 +1,74 @@
-#include <SoftwareSerial.h>
+#include "nRF24L01.h"
+#include "RF24.h"
+#include "DigitalIO.h"
 #include "crawler.h"
 #include "controller.h"
 
-SoftwareSerial BTserial(2, 3); // RX | TX
+SoftSPI<SOFT_SPI_MISO_PIN, SOFT_SPI_MOSI_PIN, SOFT_SPI_SCK_PIN, SPI_MODE> spi;
+
+RF24 radio(8, 9); // "создать" модуль на пинах 9 и 10 Для Уно
+
+byte address[][6] = {"1Node", "2Node", "3Node", "4Node", "5Node", "6Node"}; //возможные номера труб
+char receivedData[32] = "";
+
 Crawler crawler(10, 12, 11, 13, 4); // E1, M1, E2, M2
 Controller controller;
 
+String command;
 
 unsigned int xAxis = 512;
 unsigned int yAxis = 512;
-String command = "";
 
 void setup()
 {
   Serial.begin(9600);
-  BTserial.begin(9600);
   crawler.begin();
-  command.reserve(200);
+  command.reserve(32);
+
+  radio.begin(); //активировать модуль
+  radio.setAutoAck(1);         //режим подтверждения приёма, 1 вкл 0 выкл
+  radio.setRetries(0, 15);    //(время между попыткой достучаться, число попыток)
+  radio.enableAckPayload();    //разрешить отсылку данных в ответ на входящий сигнал
+  radio.setPayloadSize(32);     //размер пакета, в байтах
+
+  radio.openReadingPipe(1, address[0]);     //хотим слушать трубу 0
+  radio.setChannel(0x60);  //выбираем канал (в котором нет шумов!)
+
+  radio.setPALevel (RF24_PA_MAX); //уровень мощности передатчика. На выбор RF24_PA_MIN, RF24_PA_LOW, RF24_PA_HIGH, RF24_PA_MAX
+  radio.setDataRate (RF24_250KBPS); //скорость обмена. На выбор RF24_2MBPS, RF24_1MBPS, RF24_250KBPS
+  //должна быть одинакова на приёмнике и передатчике!
+  //при самой низкой скорости имеем самую высокую чувствительность и дальность!!
+
+  radio.powerUp(); //начать работу
+  radio.startListening(); //начинаем слушать эфир, мы приёмный модуль
 }
 
 void loop()
 {
-  while (BTserial.available())
-  {
-    char c = BTserial.read();
+  byte pipeNo;
+  if (radio.available(&pipeNo)) {   // If the NRF240L01 module received data
+    radio.read(&receivedData, sizeof(receivedData)); // Read the data and put it into character array
 
-    if (c != '\n') {
-      command += c;
-      continue;
+    command = String(receivedData);
+
+    if (command.startsWith("xy:")) {
+      char commandChars[14];
+      memset(commandChars, 0, sizeof(commandChars));
+      command.toCharArray(commandChars, sizeof(commandChars));
+      strtok(commandChars, ":,;");
+
+      xAxis = atoi(strtok (NULL, ":,;"));
+      yAxis = 1023 - atoi(strtok (NULL, ":,;"));
+
+      delay(5);
     }
-
-    if (command.startsWith("S")) {
-      yAxis = command.substring(1).toInt();
-      if (yAxis > 1023) {
-        yAxis = 1023;
-      }
-    }
-
-    if (command.startsWith("D")) {
-      xAxis = command.substring(1).toInt();
-      if (xAxis > 1023) {
-        xAxis = 1023;
-      }
-    }
-
-    if (command.startsWith("B1")) {
-      crawler.buzz();
-    }
-
-    command = "";
   }
 
   controller.setAxes(xAxis, yAxis);
 
   Crawler::Direction direction = controller.getDirection();
-  unsigned int leftSpeed = controller.getLeftSpeed();
-  unsigned int rightSpeed = controller.getRightSpeed();
+  int leftSpeed = controller.getLeftSpeed();
+  int rightSpeed = controller.getRightSpeed();
 
 
   crawler.speed(leftSpeed, rightSpeed);
